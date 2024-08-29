e4c945d379f87b60268aa1f75f97ccd1adb320a1
-martin- <martin.ferrero.thompson@gmail.com>
2024-08-29 16:29:44 +0200

    Added Multi file CMake config

--- Setup-Steps/main.cpp
+++ Setup-Steps/main.cpp
@@ -1,9 +1,13 @@
+#include "Game.h"
 #include <iostream>
 
 int main() {
 
     std::cout << "Hello Martin, Welcome to more modern C++!!" << std::endl;
 
+    Game game;
+    game.initialise();
+    
     return 0;
     
 }
---
+++ Setup-Steps/source/Game.cpp
@@ -0,0 +1,8 @@
+#include "Game.h"
+#include <iostream>
+
+void Game::initialise() {
+    
+    std::cout << "Let the Game commence!!" << std::endl;
+
+}
